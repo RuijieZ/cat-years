7df180a8710550b9136d281d38d748d40f9e5b74
jerrydream <r82zhang@uwaterloo.ca>
2015-02-20 15:35:23 -0500

    Initial Commit

---
+++ Cat Years/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cat Years
+//
+//  Created by Ruijie Zhang on 2015-02-20.
+//  Copyright (c) 2015 Ruijie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
